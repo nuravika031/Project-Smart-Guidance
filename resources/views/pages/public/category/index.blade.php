5e16f1e95bedc228e70b96284fda6fa8d8223bac
Lalu Abdurrahman <34390193+lalurahman@users.noreply.github.com>
2026-02-12 00:18:04 +0800

    icon

--- resources/views/pages/public/category/index.blade.php
+++ resources/views/pages/public/category/index.blade.php
@@ -129,7 +129,8 @@
                                             <img
                                                 src="{{ asset('storage/' . $item->icon) }}"
                                                 alt="{{ $item->name }}"
-                                                class="w-full h-full object-contain filter brightness-0 invert"
+                                                class="w-full h-full object-contain"
+                                                onerror="this.style.display='none'; this.parentElement.innerHTML='<svg xmlns=\'http://www.w3.org/2000/svg\' class=\'h-12 w-12 text-white\' fill=\'none\' viewBox=\'0 0 24 24\' stroke=\'currentColor\' stroke-width=\'2\'><path d=\'M12 14l9-5-9-5-9 5 9 5z\'/><path d=\'M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z\'/><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222\'/></svg>';"
                                             >
                                         </div>
                                     </div>
